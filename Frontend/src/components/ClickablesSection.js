e1ce87b677d5def35ca23081aaa05a8e842b57ff
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-01-21 18:53:05 +0300

    refactor: Make reusable components instead of views and styling

--- Frontend/src/components/ClickablesSection.js
+++ Frontend/src/components/ClickablesSection.js
@@ -1,17 +1,53 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { GradientButton } from './'
 
 const ClickablesSection = (props) => {
-  const propsStyles = {
-    height: props.height || '30%',
-    alignContent: props.alignContent || 'center'
+  const { container, anchor, anchorText, boldText } = styles
+
+  const submitButton = {
+    marginTop: props.marginTop || '20%',
+    marginBottom: '11%'
   }
 
 	return (
-    <View style={propsStyles}>
-      { props.children }
+    <View style={container}>
+      <View style={submitButton}>
+        <GradientButton
+          label={props.label}
+          onClick={props.onClick}
+          isLoading={props.isLoading}
+        />
+      </View>
+
+      <TouchableOpacity style={anchor} onPress={props.onPress}>
+        <Text style={anchorText}>{props.anchorText} <Text style={boldText}>{props.anchorHook}</Text></Text>
+      </TouchableOpacity>
     </View>
 	)
+}
+
+const styles = {
+  container: {
+    height: '30%',
+    alignContent: 'center'
+  },
+  anchor: {
+    alignItems: 'center',
+    height: 50,
+		justifyContent: 'center',
+		marginTop: '4%'
+  },
+  anchorText: {
+    color: 'white',
+    fontFamily: 'Roboto-Light',
+    opacity: 1,
+    fontSize: 15,
+    alignSelf: 'center'
+  },
+  boldText: {
+    fontFamily: 'Roboto-Bold',
+  }
 }
 
 export { ClickablesSection }
