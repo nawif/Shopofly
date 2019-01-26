2938779d2a2f4c88f41e8ac3e88767b3e70b7bcc
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-01-26 23:58:47 +0300

    feat: Add SaleBadge in item description

--- Frontend/src/components/ItemInfo.js
+++ Frontend/src/components/ItemInfo.js
@@ -2,8 +2,9 @@ import React from 'react'
 import {
   View,
   Text,
-  StyleSheet,
 } from 'react-native'
+
+ import { SaleBadge } from './'
 
 const ItemInfo = ({}) => {
   const item = {
@@ -12,14 +13,30 @@ const ItemInfo = ({}) => {
     specification:{ dimensions:{width:'10',height:'10'} },
     price: '2,890.00' }
 
-  const { container, sellerStyle, titleStyle, priceStyle, infoStyle, oldPriceStyle } = styles
+  const {
+    container,
+    sellerStyle,
+    titleStyle,
+    priceStyle,
+    infoStyle,
+    oldPriceStyle,
+    saleStyle
+  } = styles
+
   return (
     <View style={container}>
       <Text style={sellerStyle}>{item.seller}</Text>
+
       <Text style={titleStyle}>{item.title}</Text>
+
       <Text>TODO: Add rating/reviews here</Text>
+
       <Text style={priceStyle}>SAR {item.price} <Text style={infoStyle}>(inclusive of vat)</Text></Text>
-      <Text style={oldPriceStyle}>SAR {item.price}</Text>
+
+      <View style={saleStyle}>
+        <Text style={oldPriceStyle}>SAR {item.price}</Text>
+        <SaleBadge percentage={22} marginLeft={'2%'}/>
+      </View>
     </View>
   )
 }
@@ -47,6 +64,10 @@ const styles = {
   infoStyle: {
     color: '#dddddd',
     fontSize: 12
+  },
+  saleStyle: {
+    flexDirection: 'row',
+    marginTop: '2%'
   },
   oldPriceStyle: {
     color: 'grey',
--- Frontend/src/components/SaleBadge.js
+++ Frontend/src/components/SaleBadge.js
@@ -1,25 +1,55 @@
-/* @flow weak */
-
-import React from 'react';
+import React from 'react'
 import {
   View,
-  Text
-} from 'react-native';
+  Text,
+  Image
+} from 'react-native'
 
-const SaleBadge = ({}) => {
-  const { container } = styles
+import { LinearGradient } from 'expo'
+import * as Global from '../Global'
+
+const SaleBadge = ({ percentage, marginLeft }) => {
+  const { container, badgeStyle, offerStyle } = styles
+
+  container.marginLeft = marginLeft || null
 
   return (
-    <View style={container}>
-      <Text>%22 SALE</Text>
-    </View>
+    <LinearGradient
+      colors={[Global.FIRST_COLOR, Global.SECOND_COLOR]}
+      start={[1.0, 0]}
+      end={[0.0, 0]}
+      style={container}
+    >
+      <View style={badgeStyle}>
+        <Image
+          source={require('../../assets/percentage.png')}
+        />
+        <Text style={offerStyle}>{percentage}% OFF</Text>
+      </View>
+    </LinearGradient>
   )
 }
 
-export { SaleBadge }
-
 const styles = {
   container: {
-
+    borderRadius:40,
+    paddingLeft: 7,
+    paddingRight: 7,
+    paddingTop: 2,
+    paddingBottom: 2,
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  offerStyle: {
+    color: 'white',
+    fontSize: 10,
+    fontFamily: 'Roboto-Medium',
+    marginLeft: 3
+  },
+  badgeStyle: {
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 }
+
+export { SaleBadge }
