5e63a4dae5071534bc26160c7547787ab034bbde
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2019-02-10 15:43:02 +0300

    style: Make dots look pretty :3

--- Frontend/src/components/MySwiper.js
+++ Frontend/src/components/MySwiper.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { Text, View, Dimensions, Image } from 'react-native'
 import Swiper from 'react-native-swiper'
+import { LinearGradient } from 'expo'
+import * as Global from '../Global'
+
+const defaultDotSize = 10
 
 export default class MySwiper extends Component {
 	state = {
@@ -9,10 +13,25 @@ export default class MySwiper extends Component {
 	render() {
 		const {  } = this.state
 
-    const { container, contentStyle, slide, imageStyle } = styles
+    const { container, contentStyle, slide, imageStyle, dotStyle, activeDotStyle } = styles
 
 		return (
-      <Swiper style={container} containerStyle={contentStyle}>
+      <Swiper
+				style={container}
+				containerStyle={contentStyle}
+				dot={<View style={dotStyle} />}
+				activeDot={
+					<LinearGradient
+						colors={[Global.FIRST_COLOR, Global.SECOND_COLOR]}
+						start={[1.0, 0]}
+						end={[0.0, 0]}
+						style={activeDotStyle}
+					/>
+				}
+				paginationStyle={{
+            bottom: 0
+				}}
+			>
 				<View style={slide}>
 					<Image style={slide} source={{uri: 'http://d176tvmxv7v9ww.cloudfront.net/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone-xs-space-select-2018_av2_1_1.jpg'}} />
 				</View>
@@ -39,5 +58,24 @@ const styles = {
 		width: '70%',
 		height: '70%',
   },
+	dotStyle: {
+		backgroundColor:'rgba(0,0,0,.2)',
+		width: defaultDotSize,
+		height: defaultDotSize,
+		borderRadius: defaultDotSize/2,
+		marginLeft: 3,
+		marginRight: 3,
+		marginTop: 3,
+		marginBottom: 3,
+	},
+	activeDotStyle: {
+		width: defaultDotSize + 4,
+		height: defaultDotSize + 4,
+		borderRadius: (defaultDotSize + 4)/2,
+		marginLeft: 3,
+		marginRight: 3,
+		marginTop: 3,
+		marginBottom: 3,
+	}
 }
 export { MySwiper }
